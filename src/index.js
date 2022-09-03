ef13c878c552cd3a6293f7fb458ed7351b342f43
Bruno Negri <b.negri_@hotmail.com>
2022-09-03 07:02:21 -0300

    Requesito 1 Feito

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const path = './talker.json';
+const { join } = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,12 +11,34 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const readTalkerFile = async () => {
+  try {
+    const content = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(content);
+  } catch (error) {
+    console.log('erro na leitura', error.message);
+  }
+};
+
+const getTalkers = async () => {
+  const data = await readTalkerFile();
+  return data;
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const talkers = await getTalkers();
+  if (talkers.length < 1) {
+    return res.status(200).json([]);
+  }
+  
+  res.status(200).json(talkers);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
